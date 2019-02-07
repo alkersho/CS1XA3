9d45e4d3e1284591558824a39e91450b1968bce4
alkersho <alkersho@mcmaster.ca>
2019-02-07 01:00:06 +0000

    reformated the text and changed a few words

--- Project01/main.sh
+++ Project01/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Welcome to my main scirpt!
+read -p "Welcome to the main scirpt!
 Please choose a feature to execute(enter the corresponding number):
-  1:Merge Log
-  2:Exit
+  1: Merge Log
+  2: Exit
   " input
 
 while [[ true ]]; do
@@ -10,15 +10,16 @@ while [[ true ]]; do
     echo "execute 'megre_log.sh'"
     ./merge_log.sh
     read -p "Would you like to use another feature?
-  1:Merge Log
-  2:Exit" input
+  1: Merge Log
+  2: Exit
+" input
   elif [[ $input -eq "2" ]]; then
-    echo "Have a good day"
+    echo "Have a good day!"
     exit
   else
-    read -p "Unrecognised feature. Please choose a feature to execute(enter the corresponding number):
-  1:Merge Log
-  2:Exit
+    read -p "Unrecognised option. Please choose a feature to execute(enter the corresponding number):
+  1: Merge Log
+  2: Exit
 " input
   fi
 done
