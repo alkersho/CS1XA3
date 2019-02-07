1fd7b28cdcbb4b809ea5c5aec9cc322fc24afc98
alkersho <alkersho@mcmaster.ca>
2019-02-07 01:52:13 +0000

    created basic structure, still need to figure out commiting old versions, script still WIP

---
+++ Project01/last_wrk_cp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#python -m py_compile script.py
+#stack ghc --verbosity error script.hs
+cd ../
+read -p "Please enter the name of the file you want to fix.
+" file
+case $file in
+  *.py )
+    errors=$(python -m py_compile "$file");;
+  *.hs )
+    errors=$(stack ghc --verbosity error "$file");;
+esac
+
+if [[ -n $errors ]]; then
+  echo "The file compiles successfuly!"
+  exit
+fi
+git_log=$(git log --oneline | cut -d' ' -f1)
+for hash in $git_log ; do
+  git checkout $hash -- $file
+  case $file in
+    *.py )
+      errors=$(python -m py_compile "$file");;
+    *.hs )
+      errors=$(stack ghc --verbosity error "$file");;
+  esac
+
+  if [[ -n $errors ]]; then
+    ""
+  fi
+done
