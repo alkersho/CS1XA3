165d203ecb90a6fa20efc403facd4109b6538bf1
alkersho <alkersho@mcmaster.ca>
2019-02-12 04:06:38 +0000

    created ur_remote script to find and update remote repos

---
+++ Project01/ur_remote.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "logged in"
+mapfile -t repos < <(find . -name ".git")
+len=${#repos[@]}
+# echo "$len ${repos[@]}"
+echo "Please choose which repo to update"
+for (( i = 0; i < $len; i++ )); do
+  echo "  $i: ${repos[i]}"
+done
+read choice
+# re='^[0-9]+$'
+while [[ $((choice)) != $choice ]] || [[ ! $choice -lt $len ]]; do
+  echo "Invalid choice"
+  echo "Please choose which repo to update"
+  for (( i = 0; i < $len; i++ )); do
+    echo "$i: ${repos[i]}"
+  done
+  read choice
+done
+
+cd "${repos[$choice]}"
+echo "git pull"
+echo "FUCKING DONE"
+rm "$0"
