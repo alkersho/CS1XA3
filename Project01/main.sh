21de31181a48802a04e93b9a49ca36e769e3a163
alkersho <alkersho@mcmaster.ca>
2019-02-12 04:19:27 +0000

    added more options for more features, last wrk cp and update_repo

--- Project01/main.sh
+++ Project01/main.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-read -p "Welcome to the main scirpt!
-Please choose a feature to execute(enter the corresponding number):
+read -p "Would you like to use another feature?
   1: Merge Log
   2: TODO Log
+  3: Last Working File
+  4: Update Remote Repo
   0: Exit
 " input
 
@@ -13,7 +14,8 @@ while [[ true ]]; do
     read -p "Would you like to use another feature?
   1: Merge Log
   2: TODO Log
-  3: Last Working Version
+  3: Last Working File
+  4: Update Remote Repo
   0: Exit
 " input
   elif [[ $input -eq "2" ]]; then
@@ -22,17 +24,29 @@ while [[ true ]]; do
     read -p "Would you like to use another feature?
   1: Merge Log
   2: TODO Log
-  3: Last Working Version
+  3: Last Working File
+  4: Update Remote Repo
   0: Exit
 " input
   elif [[ $input -eq "3" ]]; then
     echo "Executing 'last_wrk_cp.sh'"
     bash ./last_wrk_cp.sh
     read -p "Would you like to use another feature?
-1: Merge Log
-2: TODO Log
-3: Last Working Version
-0: Exit
+  1: Merge Log
+  2: TODO Log
+  3: Last Working File
+  4: Update Remote Repo
+  0: Exit
+" input
+  elif [[ $input -eq "4" ]]; then
+    echo "Executing 'last_wrk_cp.sh'"
+    bash ./update_repo.sh
+    read -p "Would you like to use another feature?
+  1: Merge Log
+  2: TODO Log
+  3: Last Working File
+  4: Update Remote Repo
+  0: Exit
 " input
   elif [[ $input -eq "0" ]]; then
     echo "Have a good day!"
@@ -41,7 +55,8 @@ while [[ true ]]; do
     read -p "Unrecognised option. Please choose a feature to execute(enter the corresponding number):
   1: Merge Log
   2: TODO Log
-  3: Last Working Version
+  3: Last Working File
+  4: Update Remote Repo
   0: Exit
 " input
   fi
