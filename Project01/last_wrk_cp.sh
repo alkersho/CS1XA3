890656d147b70ac77db3eedbb4e0d03c49d889ac
alkersho <alkersho@mcmaster.ca>
2019-02-20 00:44:05 +0000

    use python 3

--- Project01/last_wrk_cp.sh
+++ Project01/last_wrk_cp.sh
@@ -9,17 +9,21 @@ done
 #check if there are any errors or no
 case $file in
   *.py )
-    errors=$(python -m py_compile "$file");;
+    errors=$(python3 -m py_compile "$file");;
   *.hs )
     errors=$(stack ghc --verbosity error "$file");;
 esac
 
 if [[ -n $errors ]]; then
+  for file in $pyc_files; do
+    git_file_log=$(git ls-files "$file")
+    if [[ -n $git_file_log ]]; then
+      rm $file
+    fi
+  done
   echo "The file compiles successfuly!"
   exit
 fi
-
-
 
 #if file doesn't work at first
 git_log=$(git log --oneline | cut -d' ' -f1)
@@ -27,7 +31,7 @@ for hash in $git_log ; do
   git checkout $hash -- $file
   case $file in
     *.py )
-      errors=$(python -m py_compile "$file");;
+      errors=$(python3 -m py_compile "$file");;
     *.hs )
       errors=$(stack ghc --verbosity error "$file");;
   esac
@@ -37,7 +41,7 @@ for hash in $git_log ; do
     echo "A working version has been found at $hash!"
     pyc_files=$(find . -name *.pyc)
     for file in $pyc_files; do
-      git_file_log=&(git ls-files "$file")
+      git_file_log=$(git ls-files "$file")
       if [[ -n $git_file_log ]]; then
         rm $file
       fi
