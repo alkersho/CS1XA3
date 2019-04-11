3962437529ecc118c1f3f0c59fd16563b31cc683
alkersho <alkersho@mcmaster.ca>
2019-04-11 19:02:01 -0400

    Added announcement model

--- Project03/v_class/classes/models.py
+++ Project03/v_class/classes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from people.models import Person
 
 
 class Class(models.Model):
@@ -8,3 +9,18 @@ class Class(models.Model):
     class Meta:
         permissions = (('can_add_class', 'Add Classes'),
                        ('can_edit_class', 'Edit Classes'))
+
+
+# announcements
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=500)
+    owner = models.ForeignKey(Person, on_delete=models.SET_NULL)
+    cl = models.ForeignKey(Class, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['cl', 'date']
+        permissions = [('can_make_announcement', 'Create Announcement'),
+                       ('can_edit_announcement', 'Edit Announcement'),
+                       ('can_delete_announcement', 'Delete Announcement')]
