32c90eb4151cb54002b1891e17143ed729fa17ee
alkersho <alkersho@mcmaster.ca>
2019-04-11 19:05:32 -0400

    Installed apps into settings.py

--- Project03/v_class/v_class/settings.py
+++ Project03/v_class/v_class/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'classes.apps.ClassesConfig',
+    'people.apps.PeopleConfig',
+    'forum.apps.ForumConfig',
 ]
 
 MIDDLEWARE = [
