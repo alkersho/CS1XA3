387b4674230d26ccf432d2caad56534db661883f
alkersho <alkersho@mcmaster.ca>
2019-04-15 23:09:40 -0400

    Cleaned register function

--- Project03/v_class/people/views.py
+++ Project03/v_class/people/views.py
@@ -12,17 +12,14 @@ def login(request):
 
 
 def register(request):
-    if request.method == "POST":
-        if request.body:
-            post = json.loads(request.body)
-            Person.objects.create_person(username=post['usrnm'],
-                                         password=post['password'],
-                                         first_name=post['fName'],
-                                         last_name=post['lName'],
-                                         email=post['email'],
-                                         dob=post['dob'],
-                                         gender=post['gender'])
-            return HttpResponse("")
-        
-        return HttpResponse("Repsons is a POST but empty")
-    return HttpResponse("Not a POST request")
+    if request.body:
+        post = json.loads(request.body)
+        Person.objects.create_person(username=post['usrnm'],
+                                     password=post['password'],
+                                     first_name=post['fName'],
+                                     last_name=post['lName'],
+                                     email=post['email'],
+                                     dob=post['dob'],
+                                     gender=post['gender'])
+        return HttpResponse("")
+    return HttpResponse("Body is Empty")
