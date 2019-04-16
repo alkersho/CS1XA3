8259d1a889c8318581eb00332e99efeaabe778dd
alkersho <alkersho@mcmaster.ca>
2019-04-15 22:58:25 -0400

    Added root var to easily port to 1XA3 server
    leave it empty for shorter urls in dev

--- Project03/v_class/v_class/urls.py
+++ Project03/v_class/v_class/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# changed later to fit server
+root = ""
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('people.urls')),
-    path('forum/', include('forum.urls')),
-    path('class/', include('classes.urls')),
+    path(root + 'admin/', admin.site.urls),
+    path(root + 'account/', include('people.urls')),
+    path(root + 'forum/', include('forum.urls')),
+    path(root + 'class/', include('classes.urls')),
 ]
