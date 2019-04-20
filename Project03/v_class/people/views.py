592f8c4ccdac5c2654c15052aadb7237be04242b
alkersho <alkersho@mcmaster.ca>
2019-04-20 16:41:57 -0400

    Implemented Account Page
    User can view their data and change email or password from account page

--- Project03/v_class/people/views.py
+++ Project03/v_class/people/views.py
@@ -2,17 +2,62 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Person
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 import json
 
 
 def main(request):
     if request.user.is_authenticated:
-        name = request.user.username
-        user_type = Person.objects.filter(
-            user__username__startswith=name).first().user_type
-        context = {"type": user_type}
+        if request.body:
+            post = json.loads(request.body)
+            email = post.get("email", "")
+            newPass = post.get("newPass", "")
+            print(post)
+            if email == "":
+                if newPass == "":
+                    return HttpResponse("Please Enter a Password!")
+                else:
+                    temp_user = authenticate(username=request.user.username,
+                                             password=post['currentPass'])
+                    print(temp_user)
+                    if temp_user is not None:
+                        u = User.objects.get(username=request.user.username)
+                        u.set_password(newPass)
+                        u.save()
+                        login(request, u)
+                        return HttpResponse("")
+                    else:
+                        return HttpResponse("Wrong Passowrd!")
+                return HttpResponse("Please Enter an email!")
+            else:
+                request.user.email = email
+                request.user.save()
+                return HttpResponse("")
+
+            return HttpResponse("")
+        username = request.user.username
+        user = Person.objects.filter(
+            user__username__startswith=username).first()
+        fName = user.first_name
+        lName = user.last_name
+        email = user.user.email
+        gender = user.get_gender_display()
+        dob = user.dob.strftime("%d/%m/%Y")
+        classes = [x.name for x in user.classes.all()]
+        user_type = user.user_type
+        type_display = user.get_user_type_display()
+        print("gender", gender)
+        print("type_display", type_display)
+        context = {"type": user_type,
+                   "typeDisplay": type_display,
+                   "fname": fName,
+                   "lname": lName,
+                   "userName": username,
+                   "email": email,
+                   "gender": gender,
+                   "classes": classes,
+                   "dob": dob}
         return render(request, 'people/account.html', context=context)
-    
     return redirect("people:login")
 
 
