0879496862fd2c0ce50bf66d7faac5dc8dde3085
alkersho <alkersho@mcmaster.ca>
2019-04-20 23:12:41 -0400

    Implemented Admin User Page
    This is different from the django admin Page,
    This page is used to change user types of all the users registered

---
+++ Project03/elm_files/admin-account.elm
@@ -0,0 +1,166 @@
+module AdminAccount exposing (main)
+
+import Http
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Bootstrap.Form.Select as Select
+import Bootstrap.Button as Button
+import Bootstrap.Table as Table
+import Bootstrap.Grid as Grid
+import Bootstrap.Alert as Alert
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Browser as Browser
+import Bootstrap.CDN as CDN
+
+main =
+  Browser.element {
+    init = init
+    , update = update
+    , view = view
+    , subscriptions = \_ -> Sub.none
+  }
+
+type alias Model = {
+    users : List User,
+    error : String
+  }
+
+type alias User = {
+  username : String,
+  userType : String
+  }
+
+type Msg =
+    ChangeType String String
+  | PostResponce (Result Http.Error String)
+  | GetResponce (Result Http.Error (List User))
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( { users = [], error = "" }, Http.get { url = "/e/alkersho/account/accounts/"
+    , expect = Http.expectJson GetResponce decodeResponce })
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        ChangeType string string2 ->
+          (model, setType string string2)
+        PostResponce stringErrorHttpResult ->
+          case stringErrorHttpResult of
+            Ok val ->
+              case val of
+                "" ->
+                  (model, Cmd.none)
+                _ ->
+                  ({model | error = val}, Cmd.none)
+            Err val ->
+              (errorHandler model val, Cmd.none)
+        GetResponce result ->
+          case result of
+            Ok val ->
+              ({model | users = val}, Cmd.none)
+            Err val ->
+              (errorHandler model val, Cmd.none)
+
+setType : String -> String -> Cmd Msg
+setType username newType =
+    Http.post {
+    url = "/e/alkersho/account/accounts/",
+    body = Http.jsonBody <| encodeRequest username newType,
+    expect = Http.expectString PostResponce
+  }
+
+encodeRequest : String -> String -> Encode.Value
+encodeRequest username newType =
+    Encode.object [
+    ("username", Encode.string username)
+    , ("newType", Encode.string newType)
+    ]
+
+decodeResponce : Decode.Decoder (List User)
+decodeResponce =
+    Decode.field "responce" <| Decode.list
+        (Decode.map2 User
+            (Decode.field "username" Decode.string)
+            (Decode.field "userType" Decode.string)
+        )
+
+
+errorHandler : Model -> Http.Error -> Model
+errorHandler model error =
+    case error of
+        Http.BadUrl url ->
+            { model | error = "bad url: " ++ url }
+
+        Http.Timeout ->
+            { model | error = "timeout" }
+
+        Http.NetworkError ->
+            { model | error = "network error" }
+
+        Http.BadStatus i ->
+            { model | error = "bad status " ++ String.fromInt i }
+
+        Http.BadBody body ->
+            { model | error = "bad body " ++ body }
+
+view : Model -> Html Msg
+view model =
+  Grid.container [] [
+    CDN.stylesheet,
+    errorView model,
+    Table.table { options = []
+    , thead = Table.thead [] [
+      Table.tr [] [
+        Table.td [] [text "User Name:"]
+        , Table.td [] [text "User Type:"]
+      ]
+    ]
+    , tbody = Table.tbody [] <| users model
+   }
+  ]
+
+errorView : Model -> Html Msg
+errorView model =
+    case model.error of
+      "" ->
+        div [] []
+      _ ->
+        Alert.simpleDanger [] [text model.error]
+
+userTypes : List (String, String)
+userTypes =
+  [
+    ("TCHR", "Teacher"),
+    ("STD", "Student"),
+    ("ADM", "Admin")
+  ]
+
+users : Model -> List (Table.Row Msg)
+users model =
+    let
+        row userList =
+          case userList of
+            x::xs ->
+              Table.tr [] [
+                Table.td [] [text x.username]
+                , Table.td [] [Select.select [ Select.onChange <| ChangeType x.username]
+                    <| items x.userType userTypes
+                ]
+              ] :: row xs
+            [] ->
+              []
+
+        items : String -> List (String, String) -> List (Select.Item Msg)
+        items cType types =
+          case types of
+            (a,b)::xs ->
+              if a == cType then
+                  Select.item [ value a, selected True] [text b] :: items cType xs
+              else
+                  Select.item [ value a, selected False] [text b] :: items cType xs
+            [] ->
+              []
+    in row model.users
--- Project03/v_class/people/views.py
+++ Project03/v_class/people/views.py
@@ -12,14 +12,12 @@ def main(request):
             post = json.loads(request.body)
             email = post.get("email", "")
             newPass = post.get("newPass", "")
-            print(post)
             if email == "":
                 if newPass == "":
                     return HttpResponse("Please Enter a Password!")
                 else:
                     temp_user = authenticate(username=request.user.username,
                                              password=post['currentPass'])
-                    print(temp_user)
                     if temp_user is not None:
                         u = User.objects.get(username=request.user.username)
                         u.set_password(newPass)
@@ -46,8 +44,6 @@ def main(request):
         classes = [x.name for x in user.classes.all()]
         user_type = user.user_type
         type_display = user.get_user_type_display()
-        print("gender", gender)
-        print("type_display", type_display)
         context = {"type": user_type,
                    "typeDisplay": type_display,
                    "fname": fName,
@@ -61,15 +57,28 @@ def main(request):
     return redirect("people:login")
 
 
+def acount_list(request):
+    if request.body:
+        post = json.loads(request.body)
+        p = Person.objects.filter(
+            user__username__startswith=post['username']).first()
+        p.user_type = post['newType']
+        p.save()
+        return HttpResponse("")
+    users_types = [{"username": x.user.username,
+                    "userType": x.user_type}
+                   for x in Person.objects.all()]
+    responce = json.dumps({"responce": users_types})
+    return HttpResponse(responce)
+
+
 def login_page(request):
     if request.body:
         post = json.loads(request.body)
-        print(post)
         user = authenticate(username=post['userName'],
                             password=post['password'])
         if user is not None:
             login(request, user)
-            print("logged in")
             return HttpResponse("")
         else:
             return HttpResponse("Wrong Username/Passowrd")
