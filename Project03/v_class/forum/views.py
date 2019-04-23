95a4e642e4e46e97a0cdc7847b06de876b2446bb
alkersho <alkersho@mcmaster.ca>
2019-04-22 20:01:58 -0400

    Created and implemented post creation page

--- Project03/v_class/forum/views.py
+++ Project03/v_class/forum/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
+from forum.models import Topic, Post
+from people.models import Person
 
 
 def main(request):
@@ -11,7 +14,24 @@ def view_post(request, post_id):
 
 
 def create_post(request):
-    return HttpResponse('Create Posts')
+    if not request.user.is_authenticated:
+        return redirect("people:login")
+    if request.method == "POST":
+        post = json.loads(request.body)
+        title = post['title']
+        body = post['body']
+        topic = Topic.objects.get(name=post['topic'])
+        user = Person.objects.get(user=request.user)
+        try:
+            new_post = Post(title=title, body=body, owner=user, topic=topic)
+            new_post.save()
+        except Exception as e:
+            print(e)
+            return HttpResponse("Failed server error.")
+        return HttpResponse("")
+    topics = [x.name for x in Topic.objects.all()]
+    return render(request, "forum/create_post.html",
+                  context={"topics": topics})
 
 
 def edit_posts(request, post_id):
